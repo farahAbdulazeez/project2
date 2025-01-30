099a305a38a63ee2e8d110363eabf2a6c43caf6b
Farah <Farah@DESKTOP-4IKORCG.lan>
2025-01-30 13:13:52 -0600

    it's fun to code with java

--- src/Demo1/DemoTest2.java
+++ src/Demo1/DemoTest2.java
@@ -4,6 +4,10 @@ public class DemoTest2 {
 	
 		public static void main(String args []) {
 			System.out.println("Java");
+			
+			String name = "fun";
+			
+			System.out.println("java is fun");
 		}
 
 }
